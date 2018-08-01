0743abf369bd47cd07368d043b42c364f7a2747c
Avill2 <andrea.villacis@epn.edu.ec>
2018-08-01 15:55:20 -0500

    completo el componenete de login

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RecepcionHotelComponent } from './recepcion-hotel/recepcion-hotel.component';
 import { RecepcionRestauranteComponent } from './recepcion-restaurante/recepcion-restaurante.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { RecepcionRestauranteComponent } from './recepcion-restaurante/recepcion
     AppComponent,
     RecepcionHotelComponent,
     RecepcionRestauranteComponent,
+    LoginComponent,
 
   ],
   imports: [
